c59a45a4b07bcdbf7dabf7387f96f9e197632532
Mx343 <Fabian-Prowald@t-online.de>
2020-10-13 13:49:59 +0200

    my first commit

---
+++ clouds_are_made_of_linux_servers/src/main/java/com/mx343/clouds_are_made_of_linux_servers/App.java
@@ -0,0 +1,13 @@
+package com.mx343.clouds_are_made_of_linux_servers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
